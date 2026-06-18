4b8cc753edbc2061f73f6351df50e87ea873b33c
Omkar Virkar <iamomkarvirkar@gmail.com>
2026-06-18 21:18:30 +0530

    Initial typeorm setup for database connectivity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { ReportsController } from './reports/reports.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot({
+    type: 'better-sqlite3', // Says what kind of database to use
+    database: 'db.sqlite',
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [AppController, ReportsController],
   providers: [AppService],
 })
