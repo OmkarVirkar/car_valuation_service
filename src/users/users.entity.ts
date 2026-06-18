92a7379e5faef44b7bfd4c9ce0a6cbd76c388f85
Omkar Virkar <iamomkarvirkar@gmail.com>
2026-06-19 04:56:29 +0530

    Adds Hooks to Entities and Injects Service and Respository for Reports into the Code flow for reports controller using dependency injection

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from "typeorm";
 
 @Entity("users")
 export class User {
@@ -10,4 +10,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert() // Log after user is inserted
+  logInsert() {
+    console.log(`User with ID ${this.id} has been inserted into the database.`);
+  }
+
+  @AfterUpdate() // Log after user is updated
+  logUpdate() {
+    console.log(`User with ID ${this.id} has been updated in the database.`);
+  }
+
+  @AfterRemove() // Log after user is removed
+  logRemove() {
+    console.log(`User with ID ${this.id} has been removed from the database.`);
+  }
 }
