c35efb4a21c7ef0402d3055c86e2a73ff825de6e
Omkar Virkar <iamomkarvirkar@gmail.com>
2026-07-06 15:53:51 +0530

    Added functions and setup for creating reports

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -2,9 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
 import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
 export class ReportsService {
     constructor(@InjectRepository(Report) private reportsRepository: Repository<Report>) {}
 
+    async createReport(reportBody: CreateReportDto): Promise<Report> {
+        const newReport = await this.reportsRepository.create(reportBody);
+        if (!newReport) {
+            throw new Error('Failed to create a new report');
+        }
+        return this.reportsRepository.save(newReport);
+    }
 }
